172754b948168e70ad37a03cb8cf2fb76388f973
fadedrt <584047548@qq.com>
2025-08-29 16:23:34 +0800

    Add files via upload

---
+++ aux_tree.R
@@ -0,0 +1,192 @@
+fill_tree_details = function(curr_tree, X) {
+  
+  # Collect right bits of tree
+  tree_matrix = curr_tree$tree_matrix
+  
+  # Create a new tree matrix to overwrite
+  new_tree_matrix = tree_matrix
+  
+  # Start with dummy node indices
+  node_indices = rep(1, nrow(X))
+  
+  # For all but the top row, find the number of observations falling into each one
+  for(i in 2:nrow(tree_matrix)) {
+    
+    # Get the parent
+    curr_parent = as.numeric(tree_matrix[i,'parent'])
+    
+    # Find the split variable and value of the parent
+    split_var = as.numeric(tree_matrix[curr_parent,'split_variable'])
+    split_val = as.numeric(tree_matrix[curr_parent, 'split_value'])
+    
+    # Find whether it's a left or right terminal node
+    left_or_right = ifelse(tree_matrix[curr_parent,'child_left'] == i,
+                           'left', 'right')
+    if(left_or_right == 'left') {
+      # If left use less than condition
+      new_tree_matrix[i,'node_size'] = sum(X[node_indices == curr_parent,split_var] < split_val)
+      node_indices[node_indices == curr_parent][X[node_indices == curr_parent,split_var] < split_val] = i
+    } else {
+      # If right use greater than condition
+      new_tree_matrix[i,'node_size'] = sum(X[node_indices == curr_parent,split_var] >= split_val)
+      node_indices[node_indices == curr_parent][X[node_indices == curr_parent,split_var] >= split_val] = i
+    }
+  } # End of loop through table
+  
+  return(list(tree_matrix = new_tree_matrix,
+              node_indices = node_indices))
+  
+} # End of function
+
+# Get predictions ---------------------------------------------------------
+
+get_predictions = function(trees, X, single_tree = FALSE) {
+  
+  # Stop nesting problems in case of multiple trees
+  if(is.null(names(trees)) & (length(trees) == 1)) trees = trees[[1]]
+  
+  # Normally trees will be a list of lists but just in case
+  if(single_tree) {
+    # Deal with just a single tree
+    if(nrow(trees$tree_matrix) == 1) {
+      predictions = rep(trees$tree_matrix[1, 'mu'], nrow(X))
+    } else {
+      # Loop through the node indices to get predictions
+      predictions = rep(NA, nrow(X))
+      unique_node_indices = unique(trees$node_indices)
+      # Get the node indices for the current X matrix
+      curr_X_node_indices = fill_tree_details(trees, X)$node_indices
+      # Now loop through all node indices to fill in details
+      for(i in 1:length(unique_node_indices)) {
+        predictions[curr_X_node_indices == unique_node_indices[i]] =
+          trees$tree_matrix[unique_node_indices[i], 'mu']
+      }
+    }
+    # More here to deal with more complicated trees - i.e. multiple trees
+  } else {
+    # Do a recursive call to the function
+    partial_trees = trees
+    partial_trees[[1]] = NULL # Blank out that element of the list
+    predictions = get_predictions(trees[[1]], X, single_tree = TRUE)  +
+      get_predictions(partial_trees, X,
+                      single_tree = length(partial_trees) == 1)
+    #single_tree = !is.null(names(partial_trees)))
+    # The above only sets single_tree to if the names of the object is not null (i.e. is a list of lists)
+  }
+  
+  return(predictions)
+}
+
+# get_children ------------------------------------------------------------
+
+get_children = function(tree_mat, parent) {
+  # Create a holder for the children
+  all_children = NULL
+  if(as.numeric(tree_mat[parent,'terminal']) == 1) {
+    # If the node is terminal return the list so far
+    return(c(all_children, parent))
+  } else {
+    # If not get the current children
+    curr_child_left = as.numeric(tree_mat[parent, 'child_left'])
+    curr_child_right = as.numeric(tree_mat[parent, 'child_right'])
+    # Return the children and also the children of the children recursively
+    return(c(all_children,
+             get_children(tree_mat,curr_child_left),
+             get_children(tree_mat,curr_child_right)))
+  }
+}
+
+# Sample function ----------------------------------------------------------
+
+resample <- function(x, ...) x[sample.int(length(x), size=1), ...]
+
+update_s  <- function(var_count, p, alpha_s) {
+  shape   <- alpha_s/p + var_count
+  temp    <- rgamma(length(shape), shape, rate=1)
+  temp/sum(temp)
+}
+
+get_number_distinct_cov <- function(tree){
+  
+  # Select the rows that correspond to internal nodes
+  which_terminal = which(tree$tree_matrix[,'terminal'] == 0)
+  # Get the covariates that are used to define the splitting rules
+  num_distinct_cov = length(unique(tree$tree_matrix[which_terminal,'split_variable']))
+  
+  return(num_distinct_cov)
+}
+
+sample_move = function(curr_tree, i, nburn){
+  
+  if (nrow(curr_tree$tree_matrix) == 1 || i < max(floor(0.1*nburn), 10)) {
+    type = 'grow'
+  } else {
+    type = sample(c('grow', 'prune', 'change'), 1)
+  }
+  return(type)
+}
+get_ancestors = function(tree){
+  
+  save_ancestor = NULL
+  which_terminal = which(tree$tree_matrix[,'terminal'] == 1)
+  
+  if(nrow(tree$tree_matrix) == 1) {
+    save_ancestor = cbind(terminal = NULL,
+                          ancestor = NULL)
+  } else {
+    for (k in seq_len(length(which_terminal))) {
+      get_parent = as.numeric(as.character(tree$tree_matrix[which_terminal[k], 'parent'])) # get the 1st parent
+      get_split_var = as.character(tree$tree_matrix[get_parent, 'split_variable']) # then, get the covariate associated to the row of the parent
+      
+      save_ancestor = rbind(save_ancestor,
+                            cbind(terminal = which_terminal[k],
+                                  # parent   = get_parent,
+                                  ancestor = get_split_var))
+      while (get_parent > 1){
+        get_parent = as.numeric(as.character(tree$tree_matrix[get_parent,'parent'])) # then, get the subsequent parent
+        get_split_var = as.character(tree$tree_matrix[get_parent, 'split_variable']) # then, get the covariate associated to the row of the new parent
+        save_ancestor = rbind(save_ancestor,
+                              cbind(terminal = which_terminal[k],
+                                    # parent   = get_parent,
+                                    ancestor = get_split_var))
+      }
+    }
+    save_ancestor = unique(save_ancestor) # remove duplicates
+    save_ancestor = save_ancestor[order(save_ancestor[,1], save_ancestor[,2]),] # sort by terminal and ancestor
+  }
+  
+  return(save_ancestor)
+}
+
+get_ancestors_internal = function(tree){
+  save_ancestor = NULL
+  tree = tree$tree_matrix
+  which_internal = which(tree[,'terminal'] == 0)
+  
+  
+  if(nrow(tree) == 1) {
+    save_ancestor = cbind(internal = NULL,
+                          ancestor = NULL)
+  } else {
+    for (k in length(which_internal):1) {
+      internal_node = which_internal[k]
+      parent = tree[internal_node, 'parent']
+      get_split_var = tree[internal_node, 'split_variable']
+      
+      save_ancestor = rbind(save_ancestor,
+                            cbind(internal = internal_node,
+                                  parent   = parent,
+                                  split_var = get_split_var))
+      while (is.na(parent) == FALSE && parent > 0) {
+        get_split_var = tree[parent, 'split_variable']
+        parent = tree[parent, 'parent']
+        save_ancestor = rbind(save_ancestor,
+                              cbind(internal = internal_node,
+                                    parent   = parent,
+                                    split_var = get_split_var))
+      }
+    }
+  }
+  return(save_ancestor[,,drop=FALSE])
+  
+}
